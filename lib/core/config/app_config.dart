62e9239d278ac6535c82ca47926308ea931dc661
Lasith Dilshan Ranasinghe <luminafyp@gmail.com>
2026-02-11 20:21:40 +0530

    feat: Update base URL for API requests and improve navigation in WireframeCameraScreen

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -3,5 +3,5 @@ class AppConfig {
   AppConfig._();
 
   /// Base URL for all API requests
-  static const String baseUrl = 'http://10.64.175.112:8000/api'; // Update as needed
+  static const String baseUrl = 'http://10.118.182.112:8000/api'; // Update as needed
 }
